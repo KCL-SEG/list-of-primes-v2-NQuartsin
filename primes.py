a7a677818b6e6a3f8aac04f791dec37c76eb6eeb
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-03 01:01:32 +0100

    Fix logic error for finding primes.

--- primes.py
+++ primes.py
@@ -5,11 +5,15 @@ def primes(number_of_primes):
     if (number_of_primes <=0):
         raise ValueError(f"{number_of_primes}, should have been a positive number.")
     list = []
-    for i in range (2,number_of_primes):
+    counter = 0
+    i = 2
+    while(counter < number_of_primes):
         prime = True
         for j in range (2,i):
             if (i%j == 0):
                 prime = False
         if (prime = True):
             list.append(i)
+            counter = counter + 1
+        i = i + 1
     return list
