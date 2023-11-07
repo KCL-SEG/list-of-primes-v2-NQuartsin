417ae9fcc6d26c4eb0354a9260ca4084c96d9638
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-11-07 16:58:38 +0000

    Fix inconsistent variable name i

--- primes.py
+++ primes.py
@@ -6,14 +6,13 @@ def primes(number_of_primes):
         raise ValueError(f"{number_of_primes}, should have been a positive number.")
     list = []
     
-    #could use more indicative variable names than i and j. I guess
-    possible_prime = 2 
+    i = 2 # where i is a possible prime number
 
     #could place a comment here to explain the purpose of the while loop
     while(len(list) < number_of_primes):
         isPrime = True
         for j in range (2,i): # where j is a possible factor of i
-            if (possible_prime % j == 0):
+            if (i % j == 0):
                 prime = False
         if (prime == True):
             list.append(i)
