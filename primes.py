d7006023055428756c4efeaf7cfd76931dc51cbd
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-11-07 16:56:20 +0000

    Change variable names, fix 'prime = True'

--- primes.py
+++ primes.py
@@ -5,15 +5,17 @@ def primes(number_of_primes):
     if (number_of_primes <=0):
         raise ValueError(f"{number_of_primes}, should have been a positive number.")
     list = []
-    counter = 0
-    i = 2
-    while(counter < number_of_primes):
-        prime = True
-        for j in range (2,i):
-            if (i%j == 0):
+    
+    #could use more indicative variable names than i and j. I guess
+    possible_prime = 2 
+
+    #could place a comment here to explain the purpose of the while loop
+    while(len(list) < number_of_primes):
+        isPrime = True
+        for j in range (2,i): # where j is a possible factor of i
+            if (possible_prime % j == 0):
                 prime = False
-        if (prime = True):
+        if (prime == True):
             list.append(i)
-            counter = counter + 1
         i = i + 1
     return list
